19b1c921937028c43732b655d1b1d52eb116d6c2
ironicmani <manikanta.pabi@gmail.com>
2023-10-26 23:01:34 +0530

    ADD Factorial_calculator.c

---
+++ c/Factorial_calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %lld\n", num, factorial(num));
+    }
+
+    return 0;
+}
